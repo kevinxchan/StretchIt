6f3f39243817c813b6de2d33ecd5b17f8b133641
Kevin Chan <kevchan1@alumni.ubc.ca>
2018-07-17 21:06:00 -0700

    starter activity transition

--- app/src/main/java/me/kevinxchan/kevinxchan/stretchit/MainActivity.java
+++ app/src/main/java/me/kevinxchan/kevinxchan/stretchit/MainActivity.java
@@ -1,13 +1,10 @@
 package me.kevinxchan.kevinxchan.stretchit;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,15 +15,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Stretch It");
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
--- app/src/main/java/me/kevinxchan/kevinxchan/stretchit/StarterActivity.java
+++ app/src/main/java/me/kevinxchan/kevinxchan/stretchit/StarterActivity.java
@@ -1,8 +1,11 @@
 package me.kevinxchan.kevinxchan.stretchit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class StarterActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class StarterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_starter);
+
+        Button getStartedBtn = (Button) findViewById(R.id.getStartedBtn);
+        getStartedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
