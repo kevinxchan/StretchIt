b32033ee1790c4593e5814f7c993391ebf79d9c6
Kevin Chan <kevchan1@alumni.ubc.ca>
2018-07-18 21:12:30 -0700

    check for required routine name

--- app/src/main/java/me/kevinxchan/kevinxchan/stretchit/CreateRoutinesActivity.java
+++ app/src/main/java/me/kevinxchan/kevinxchan/stretchit/CreateRoutinesActivity.java
@@ -3,23 +3,37 @@ package me.kevinxchan.kevinxchan.stretchit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateRoutinesActivity extends AppCompatActivity {
+    EditText routineNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_routines);
 
+        routineNameEditText = (EditText) findViewById(R.id.routineNameEditText);
         Button nextBtn = (Button) findViewById(R.id.nextBtn);
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent nextActivity = new Intent(getApplicationContext(), AddExercisesActivity.class);
-                startActivity(nextActivity);
+                if (editTextNotEmpty(routineNameEditText)) {
+                    Log.d("foo", "I AM NOT EMPTY");
+                    Intent nextActivity = new Intent(getApplicationContext(), AddExercisesActivity.class);
+                    startActivity(nextActivity);
+                } else {
+                    routineNameEditText.setError("Routine name is required!");
+                }
             }
         });
+    }
+
+    private boolean editTextNotEmpty(EditText editText) {
+        return !(TextUtils.isEmpty(editText.getText()));
     }
 }
