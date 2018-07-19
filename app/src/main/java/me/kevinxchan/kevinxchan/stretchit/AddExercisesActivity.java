b2f606d51d94b772342dd2a1cb2123e47beafbf1
Kevin Chan <kevchan1@alumni.ubc.ca>
2018-07-18 21:01:28 -0700

    add links to new activities and stubs

--- app/src/main/java/me/kevinxchan/kevinxchan/stretchit/AddExercisesActivity.java
+++ app/src/main/java/me/kevinxchan/kevinxchan/stretchit/AddExercisesActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import com.github.clans.fab.FloatingActionButton;
 
 public class AddExercisesActivity extends AppCompatActivity {
 
@@ -14,6 +15,9 @@ public class AddExercisesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_exercises);
 
         Button doneBtn = (Button) findViewById(R.id.doneBtn);
+        FloatingActionButton floatingActionButtonCountdown = (FloatingActionButton) findViewById(R.id.floatingActionItemCountdown);
+        FloatingActionButton floatingActionButtonRest = (FloatingActionButton) findViewById(R.id.floatingActionItemRest);
+        FloatingActionButton floatingActionButtonExercise = (FloatingActionButton) findViewById(R.id.floatingActionItemExercise);
 
         doneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,11 +26,26 @@ public class AddExercisesActivity extends AppCompatActivity {
                 startActivity(doneIntent);
             }
         });
-//        addExerciseFAB.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent startAddContentIntent = new Intent(getApplicationContext(), AddContent.class);
-//            }
-//        });
+        floatingActionButtonCountdown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent countDownIntent = new Intent(getApplicationContext(), CountdownActivity.class);
+                startActivity(countDownIntent);
+            }
+        });
+        floatingActionButtonRest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent restIntent = new Intent(getApplicationContext(), RestActivity.class);
+                startActivity(restIntent);
+            }
+        });
+        floatingActionButtonExercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent exerciseIntent = new Intent(getApplicationContext(), ExerciseActivity.class);
+                startActivity(exerciseIntent);
+            }
+        });
     }
 }
