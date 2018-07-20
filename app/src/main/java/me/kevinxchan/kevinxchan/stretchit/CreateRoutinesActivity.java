17df48427c71411b024e9fdaccc0eb3a580d8dac
Kevin Chan <kevchan1@alumni.ubc.ca>
2018-07-19 20:08:51 -0700

    remove log

--- app/src/main/java/me/kevinxchan/kevinxchan/stretchit/CreateRoutinesActivity.java
+++ app/src/main/java/me/kevinxchan/kevinxchan/stretchit/CreateRoutinesActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,7 +22,6 @@ public class CreateRoutinesActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (editTextNotEmpty(routineNameEditText)) {
-                    Log.d("foo", "I AM NOT EMPTY");
                     Intent nextActivity = new Intent(getApplicationContext(), AddExercisesActivity.class);
                     startActivity(nextActivity);
                 } else {
