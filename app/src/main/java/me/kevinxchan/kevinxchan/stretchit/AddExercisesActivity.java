2da49348c74624bf1b661acc1c593825c5f98475
Kevin Chan <kevchan1@alumni.ubc.ca>
2018-07-19 21:30:18 -0700

    add room db and exercise entity setup

--- app/src/main/java/me/kevinxchan/kevinxchan/stretchit/AddExercisesActivity.java
+++ app/src/main/java/me/kevinxchan/kevinxchan/stretchit/AddExercisesActivity.java
@@ -3,25 +3,42 @@ package me.kevinxchan.kevinxchan.stretchit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import com.github.clans.fab.FloatingActionButton;
+import me.kevinxchan.kevinxchan.stretchit.adapters.ExerciseAdapter;
+import me.kevinxchan.kevinxchan.stretchit.model.Exercise;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddExercisesActivity extends AppCompatActivity {
     EditText routineNameEditText;
+    RecyclerView recyclerViewExercises;
+    RecyclerView.Adapter adapter;
+
+    List<Exercise> exercises;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_exercises);
 
+        exercises = new ArrayList<Exercise>();
+        recyclerViewExercises = findViewById(R.id.recyclerViewExercises);
+        recyclerViewExercises.setLayoutManager(new LinearLayoutManager(null));
+        adapter = new ExerciseAdapter(exercises);
+        recyclerViewExercises.setAdapter(adapter);
         routineNameEditText = (EditText) findViewById(R.id.routineNameEditText);
         Button doneBtn = (Button) findViewById(R.id.doneBtn);
         FloatingActionButton floatingActionButtonCountdown = (FloatingActionButton) findViewById(R.id.floatingActionItemCountdown);
         FloatingActionButton floatingActionButtonRest = (FloatingActionButton) findViewById(R.id.floatingActionItemRest);
         FloatingActionButton floatingActionButtonExercise = (FloatingActionButton) findViewById(R.id.floatingActionItemExercise);
+
 
         doneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/me/kevinxchan/kevinxchan/stretchit/model/Exercise.java
@@ -0,0 +1,6 @@
+package me.kevinxchan.kevinxchan.stretchit.model;
+
+public class Exercise {
+    // TODO: implement
+    // id, category (rest, countdown, exercise), name
+}
