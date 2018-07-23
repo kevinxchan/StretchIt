0a7bca43fbb9b59860f41b803b67d7014e2ba9bb
Kevin Chan <kevchan1@alumni.ubc.ca>
2018-07-22 17:47:49 -0700

    boilerplate code for routine adapter

--- app/src/main/java/me/kevinxchan/kevinxchan/stretchit/adapters/RoutineAdapter.java
+++ app/src/main/java/me/kevinxchan/kevinxchan/stretchit/adapters/RoutineAdapter.java
@@ -1,5 +1,38 @@
 package me.kevinxchan.kevinxchan.stretchit.adapters;
 
-public class RoutineAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import me.kevinxchan.kevinxchan.stretchit.model.Routine;
+
+import java.util.List;
+
+public class RoutineAdapter extends RecyclerView.Adapter<RoutineAdapter.ViewHolder> {
     // TODO: implement
+    private List<Routine> routines;
+
+    public RoutineAdapter(List<Routine> routines) {
+        this.routines = routines;
+    }
+
+    @Override
+    public RoutineAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RoutineAdapter.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
