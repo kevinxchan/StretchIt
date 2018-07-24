9dd55831dac32f2c4df7ac2b5a48f0dc9a748a3d
Kevin Chan <kevchan1@alumni.ubc.ca>
2018-07-24 12:04:43 -0700

    start implementation of routine and dao

--- app/src/main/java/me/kevinxchan/kevinxchan/stretchit/db/RoutineDao.java
+++ app/src/main/java/me/kevinxchan/kevinxchan/stretchit/db/RoutineDao.java
@@ -1,6 +1,9 @@
 package me.kevinxchan.kevinxchan.stretchit.db;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 import me.kevinxchan.kevinxchan.stretchit.model.Routine;
 
@@ -9,7 +12,14 @@ import java.util.List;
 @Dao
 public interface RoutineDao {
     @Query("SELECT * FROM routine")
-    List<Routine> getAllRoutines();
+    LiveData<List<Routine>> getAllRoutines();
 
-    // TODO: implement!
+    @Query("SELECT * FROM routine WHERE name = :name")
+    Routine findRoutineByName(String name);
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    void insert(Routine routine);
+
+    @Query("UPDATE routine SET name = :setToName WHERE name = :originalName")
+    void updateRoutine(String setToName, String originalName);
 }
--- app/src/main/java/me/kevinxchan/kevinxchan/stretchit/model/Routine.java
+++ app/src/main/java/me/kevinxchan/kevinxchan/stretchit/model/Routine.java
@@ -2,20 +2,24 @@ package me.kevinxchan.kevinxchan.stretchit.model;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
 
-@Entity
+@Entity(tableName = "routine", indices = { @Index(value = "name", unique = true) })
 public class Routine {
     @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "rid")
     private int routineID;
 
     @ColumnInfo(name = "name")
+    @NonNull
     private String name;
 
     @ColumnInfo(name = "num_times_used")
     private int numTimesUsed;
 
-    public Routine(String name) {
+    public Routine(@NonNull String name) {
         this.name = name;
         this.numTimesUsed = 0;
     }
