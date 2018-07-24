c883e56c8b6cee545f46e25d95e9d97ae31b1bf9
Kevin Chan <kevchan1@alumni.ubc.ca>
2018-07-24 13:35:35 -0700

    implement app db

--- app/src/main/java/me/kevinxchan/kevinxchan/stretchit/db/AppDatabase.java
+++ app/src/main/java/me/kevinxchan/kevinxchan/stretchit/db/AppDatabase.java
@@ -1,11 +1,26 @@
 package me.kevinxchan.kevinxchan.stretchit.db;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import me.kevinxchan.kevinxchan.stretchit.model.Exercise;
 import me.kevinxchan.kevinxchan.stretchit.model.Routine;
 
-@Database(entities = { Routine.class }, version = 1)
+@Database(entities = { Routine.class, Exercise.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
+    private static AppDatabase instance;
+    private static final String dbName = "routines.db";
+
     public abstract RoutineDao routineDao();
-    // TODO: implement!
+    public abstract ExerciseDao exerciseDao();
+
+    public static AppDatabase getInstance(final Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, dbName).build();
+        }
+        return instance;
+    }
+
+
 }
