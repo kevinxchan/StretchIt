5c4748847597e8b20fc39c070cba8051a8a894ce
Kevin Chan <kevchan1@alumni.ubc.ca>
2018-07-25 16:55:45 -0700

    add type converters for Category

--- app/src/main/java/me/kevinxchan/kevinxchan/stretchit/db/AppDatabase.java
+++ app/src/main/java/me/kevinxchan/kevinxchan/stretchit/db/AppDatabase.java
@@ -3,11 +3,14 @@ package me.kevinxchan.kevinxchan.stretchit.db;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
 import android.content.Context;
+import me.kevinxchan.kevinxchan.stretchit.model.Converter;
 import me.kevinxchan.kevinxchan.stretchit.model.Exercise;
 import me.kevinxchan.kevinxchan.stretchit.model.Routine;
 
 @Database(entities = { Routine.class, Exercise.class}, version = 1)
+@TypeConverters({ Converter.class })
 public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase instance;
     private static final String dbName = "routines.db";
