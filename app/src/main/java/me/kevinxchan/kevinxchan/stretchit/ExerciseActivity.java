f9ac9fdad1301bb047fb37032066a5b7b288adad
Kevin Chan <kevchan1@alumni.ubc.ca>
2018-07-28 15:41:29 -0700

    update exercise spec ui with fab

--- app/src/main/java/me/kevinxchan/kevinxchan/stretchit/ExerciseActivity.java
+++ app/src/main/java/me/kevinxchan/kevinxchan/stretchit/ExerciseActivity.java
@@ -5,36 +5,23 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 import com.aigestudio.wheelpicker.WheelPicker;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ExerciseActivity extends AppCompatActivity {
-    private List<String> exerciseCategories;
-    private List<String> hoursScroll;
-    private List<String> minutesScroll;
-    private List<String> secondsScroll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
+        setToolbar(getString(R.string.activity_exercises_title));
 
-        Button doneBtn = (Button) findViewById(R.id.doneBtn);
-        WheelPicker exerciseCategoryPicker = (WheelPicker) findViewById(R.id.exerciseCategoryWheelPicker);
-        WheelPicker hoursWheelPicker = (WheelPicker) findViewById(R.id.hoursWheelPicker);
-        WheelPicker minutesWheelPicker = (WheelPicker) findViewById(R.id.minutesWheelPicker);
-        WheelPicker secondsWheelPicker = (WheelPicker) findViewById(R.id.secondsWheelPicker);
+        initFormData();
 
-        initializeExerciseCategoryPicker(exerciseCategoryPicker);
-        hoursScroll = initializeTimeArray(0, 99);
-        minutesScroll = initializeTimeArray(0, 59);
-        secondsScroll = initializeTimeArray(0, 59);
-        hoursWheelPicker.setData(hoursScroll);
-        minutesWheelPicker.setData(minutesScroll);
-        secondsWheelPicker.setData(secondsScroll);
-
+        Button doneBtn = findViewById(R.id.doneBtn);
         doneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,6 +31,27 @@ public class ExerciseActivity extends AppCompatActivity {
                 startActivity(doneIntent);
             }
         });
+    }
+
+    private void initFormData() {
+        WheelPicker exerciseCategoryPicker = (WheelPicker) findViewById(R.id.exerciseCategoryWheelPicker);
+        WheelPicker hoursWheelPicker = (WheelPicker) findViewById(R.id.hoursWheelPicker);
+        WheelPicker minutesWheelPicker = (WheelPicker) findViewById(R.id.minutesWheelPicker);
+        WheelPicker secondsWheelPicker = (WheelPicker) findViewById(R.id.secondsWheelPicker);
+
+        initializeExerciseCategoryPicker(exerciseCategoryPicker);
+        List<String> hoursScroll = initializeTimeArray(0, 99);
+        List<String> minutesScroll = initializeTimeArray(0, 59);
+        List<String> secondsScroll = initializeTimeArray(0, 59);
+
+        hoursWheelPicker.setData(hoursScroll);
+        minutesWheelPicker.setData(minutesScroll);
+        secondsWheelPicker.setData(secondsScroll);
+    }
+
+    private void setToolbar(String string) {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(string);
     }
 
     private List<String> initializeTimeArray(int start, int end) {
@@ -59,7 +67,7 @@ public class ExerciseActivity extends AppCompatActivity {
     }
 
     private void initializeExerciseCategoryPicker(WheelPicker exerciseCategoryPicker) {
-        exerciseCategories = new ArrayList<>();
+        List<String> exerciseCategories = new ArrayList<>();
         exerciseCategories.add("Countdown");
         exerciseCategories.add("Exercise");
         exerciseCategories.add("Rest");
